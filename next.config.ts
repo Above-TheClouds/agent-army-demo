85b75c7f850f22f2afba47367eb0171c5372550d
n:ko <nicolas.lair@gmail.com>
2026-06-13 15:58:04 +0100

    fix: build error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 import { withSentryConfig } from "@sentry/nextjs";
 
-const config: NextConfig = {
-  experimental: { instrumentationHook: true },
-};
+const config: NextConfig = {};
 
 export default withSentryConfig(config, {
   silent: true,
